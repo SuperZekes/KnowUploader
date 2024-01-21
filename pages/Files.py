7ff8fdb68fcd4c07dd9fb4abb953de48e24ae588
BuildTools <unconfigured@null.spigotmc.org>
2024-01-21 19:03:35 -0400

    Added message for when you have no files uploaded

--- pages/Files.py
+++ pages/Files.py
@@ -2,20 +2,23 @@ import os
 import streamlit as st
 from pathlib import Path
 
-uploaded_files_dir = 'uploaded_files' 
+uploaded_files_dir = 'uploaded_files'
 
-st.set_page_config(page_title="KnowUploader | Files" , page_icon="📂")
+st.set_page_config(page_title="KnowUploader | Files", page_icon="📂") 
 
 st.header('Files')
 
-files = os.listdir(uploaded_files_dir)
+if not os.path.exists(uploaded_files_dir):
+    st.write("You have not uploaded any files yet!")
+else:
+    files = os.listdir(uploaded_files_dir)
 
-for file in files:
-    file_path = os.path.join(uploaded_files_dir,file)
-    with open(file_path,"rb") as f:
-        btn = st.download_button(
-            label=""+file, 
-            data=f,
-            file_name=file,
-            mime="application/octet-stream"
-        )
+    for file in files:
+        file_path = os.path.join(uploaded_files_dir,file)
+        with open(file_path,"rb") as f:
+            btn = st.download_button(
+                label=""+file,
+                data=f,
+                file_name=file,
+                mime="application/octet-stream"
+            )
